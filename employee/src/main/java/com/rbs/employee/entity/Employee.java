da060f69617c927a3c33fd2893b5245175c5bb05
Nitya.Vij <Nitya.Vij@IONDELWS759.iontrading.com>
2019-07-05 12:23:45 +0530

    Initial Commit

---
+++ employee/src/main/java/com/rbs/employee/entity/Employee.java
@@ -0,0 +1,17 @@
+package com.rbs.employee.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	long id;
+	
+	String name;
+
+}
